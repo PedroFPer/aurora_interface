f87a5d08ed9e7f26a98f65c526b7ee2b5a09d1ca
JohnWilliam24dev <johnwilliam24.dev@gmail.com>
2025-05-30 21:00:55 -0300

    não mexe

--- Infrastructure/Service/LoginFuncionarioService.js
+++ Infrastructure/Service/LoginFuncionarioService.js
@@ -1,16 +1,14 @@
 import { FuncionarioGateway } from "../Gateway/FuncionarioGateway.js"
-import { Funcionario } from "../Entities/Funcionario.js"
+
 
 export class LoginFuncionarioService{
     
-    constructor(cpf,password){
-        this.funcionarioGateway = new FuncionarioGateway()
-        this.cpf=cpf
-        this.password=password
+    constructor(){
+        this.funcionarioGateway= new FuncionarioGateway()
     }
-    async verificar_login() {
-    const funcionario = await this.funcionarioGateway.getByCpf(this.cpf);
-    if (funcionario && funcionario.senha === this.password) {
+    async verificar_login(cpf,senha) {
+    const funcionario = await this.funcionarioGateway.getByCpf(cpf);
+    if (funcionario && funcionario.senha === senha) {
         console.log("Login válido");
         return true
     } else {
