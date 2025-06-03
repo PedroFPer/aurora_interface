b91aa355db7d4bba9ac48f3138a2e1b41718156d
Pedro <pedro.f.pereira@ba.estudante.senai.br>
2025-06-03 07:06:49 -0300

    Ajustes

--- Infrastructure/Persistence/VericacaoAcesso.js
+++ Infrastructure/Persistence/VericacaoAcesso.js
@@ -1,8 +1,10 @@
 import { verificarPersistence } from "./VerificarPersistence.js";
 
+
 const clienteInfo = verificarPersistence();
 const pathname = window.location.pathname;
-const pageName = pathname.split("/").pop(); 
+const pageName = pathname.split("/").pop();
+
 
 const permissoes = {
   geral: [
@@ -21,6 +23,14 @@ const permissoes = {
     "tela_produto_consumido_produto_7.html",
   ],
 
+
+  paginasDeLoginOuCadastro: [
+    "tela_login_funcionario.html",
+    "tela_login_consumidor.html",
+    "tela_cadastro_consumidor.html"
+  ],
+
+
   costumer: [
     "consumer.tela_adicionar_endereco.html",
     "consumer.tela_carrinho.html",
@@ -29,42 +39,65 @@ const permissoes = {
     "consumer.tela_pedidos.html"
   ],
 
+
   ADMINISTRADOR_GERAL: [
     "employer.tela_edicao_produto.html",
     "employer.tela_cadastro_funcionario.html",
     "employer.tela_adicionar_produto.html"
   ],
 
+
   GERENCIADOR_ROUPAS: [
     "employer.tela_edicao_produto.html",
     "employer.tela_adicionar_produto.html"
   ],
+
 
   GERENCIADOR_FUNCIONARIOS: [
     "employer.tela_cadastro_funcionario.html"
   ]
 };
 
+
+
+
+
+
 function temPermissao(clienteInfo, pageName) {
+  if (
+    clienteInfo !== "deslogado" &&
+    permissoes.paginasDeLoginOuCadastro.includes(pageName)
+  ) {
+    window.location.href = "/pages/index.html";
+    return false;
+  }
+
+
   if (permissoes.geral.includes(pageName)) {
     return true;
   }
 
+
   if (clienteInfo === "deslogado") {
+    permissoes.paginasDeLoginOuCadastro.includes(pageName)
     return false;
   }
+
 
   if (clienteInfo.userType === "costumer") {
     return permissoes.costumer.includes(pageName);
   }
+
 
   if (clienteInfo.userType === "employer") {
     const cargo = clienteInfo.cargo;
     return permissoes[cargo]?.includes(pageName) || false;
   }
 
+
   return false;
 }
+
 
 if (!temPermissao(clienteInfo, pageName)) {
   window.location.href = "/pages/acesso-negado.html";
